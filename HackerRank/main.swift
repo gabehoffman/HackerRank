23efa14d7a666abadf1dcbb6cbf14f2076a1c885
Gabe Hoffman <gabehoffman@me.com>
2017-07-17 11:52:36 -0400

    Initial Commit

---
+++ HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Gabe Hoffman on 7/17/17.
+//  Copyright © 2017 Hoffman Tools. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
